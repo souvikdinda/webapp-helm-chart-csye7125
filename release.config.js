cb435c5fbf3e52a2170d0a32847de3bbfd2472fb
Souvik Dinda <souvik.dinda.sd@gmail.com>
2023-10-26 23:12:47 -0400

    fix: Fixed release config

--- release.config.js
+++ release.config.js
@@ -1,5 +1,5 @@
 const config = {
-    branches: ['*'],
+    branches: ["main", "origin/main"],
     plugins: [
         "@semantic-release/commit-analyzer",
         "@semantic-release/release-notes-generator",
